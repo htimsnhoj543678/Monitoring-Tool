37f6cf5adcf0841c98abe3c91269a6b669622f31
htimsnhoj543678 <54507190+htimsnhoj543678@users.noreply.github.com>
2021-11-20 16:30:54 -0500

    Update Node.java

--- src/monitoring/tool/Node.java
+++ src/monitoring/tool/Node.java
@@ -7,15 +7,17 @@ public class Node
     private String name;                                            //name of the node eg: "Hong Kong"
     private double lat;                                             //type may be changed, will be latitude on map
     private double lon;                                             //type may be changed, will be longitude on map
+    private int xpos;
+    private int ypos;
     private boolean onlineStatus = true;                            //default true or false if node has 6 or more injections with a min of 2 types of viruses
     private boolean firewallStatus;                                 //true (stops attacks)
     private boolean outbreakStatus = false;                         //default false or true if node has more than 4 injections of the same virus in 4 mins
     private int numOfConnections;                                   //number of other nodes this node is connected to
-    private Node[] connections;                                     //list of connected nodes
-    private Attack[] attacks;                                       //list of attacks
-    private ArrayList<String> attackList = new ArrayList<String>(); //type of attack: red, blue, yellow, or black, can then count # of attacks from this
+    private ArrayList<String> connections = new ArrayList<>();                                     //list of connected nodes
+    //private Attack[] attacks;                                       //list of attacks
+    //private ArrayList<String> attackList = new ArrayList<String>(); //type of attack: red, blue, yellow, or black, can then count # of attacks from this
     private int numOfAttacks;                                       //number of attacks on this node
-    private ArrayList<Node> badConnections;                         //list of connected nodes which are infected
+    //private ArrayList<Node> badConnections;                         //list of connected nodes which are infected
     //private ArrayList<Attack> firewallLog;                        //list of previous attacks if 'firewall' is set to true
 
     //constructor
@@ -61,50 +63,26 @@ public class Node
     public String getName() {return this.name;}
     public int getLon() {return (int)this.lon;}
     public int getLat() {return (int)this.lat;}
-    public Node[] getConnections() {return connections;}
-    public void printConnections()
-    {
-        for(int i = 0; i <connections.length; i++)
-        {
-            if(connections[i] != null)
-            System.out.println(connections[i].name);
-        }
-    }
+    public int getXpos() {return xpos;}
+    public int getYpos() {return ypos;}
+    public ArrayList<String> getConnections() {return connections;}
+
     //public String getAttackType() {return attackType;}
     public int getNumOfConnections() {return numOfConnections;}
     public int getNumOfAttacks() {return numOfAttacks;}
-    public ArrayList getBadConnections() {return badConnections;}
+    //public ArrayList getBadConnections() {return badConnections;}
     //public ArrayList getFirewallLog() {return firewallLog;}
 
     //setters
     public void setOnlineStatus(boolean status) {this.onlineStatus = status;}
-    public void setLonLat(double lat, double lon){this.lat=lat; this.lon=lon;}
     public void setFirewallStatus(boolean status) {this.firewallStatus = status;}
     public void setOutbreakStatus(boolean status) {this.outbreakStatus = status;}
-    public void setNumConnections(int size)
-    {
-        this.connections = new Node[size];
-        Arrays.fill(this.connections, null);
-    }
+    public void setXY(int x, int y){this.xpos=x; this.ypos=y;}
 
     //functions
-    public void insertConnection(Node node)
+    public void insertConnection(String node)
     {
-        for(int i = 0; i<connections.length; i++)
-        {
-            if(this.connections[i] == null)
-            {
-                this.connections[i] = node;
-                System.out.println("Inserted "+node.getName()+" at index: " +i);
-                break;
-            }
-        }
+        this.connections.add(node);
     }
 
-    public ArrayList<String> addInjection(String colorType)
-    {
-        this.numOfAttacks++;
-        this.attackList.add(colorType);
-        return attackList;
-    }
 }
