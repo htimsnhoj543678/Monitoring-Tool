28a55b0ef181a2130ac9c40315056ea1b92b8c49
htimsnhoj543678 <54507190+htimsnhoj543678@users.noreply.github.com>
2021-11-22 18:50:51 -0500

    Add files via upload

---
+++ src/monitoring/tool/CityName.java
@@ -0,0 +1,22 @@
+package monitoring.tool;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CityName extends JLabel
+{
+    private String name;
+    private int xPos;
+    private int yPos;
+
+    public CityName(String name, int xPos, int yPos) {this.name = name; this.xPos = xPos; this.yPos = yPos;}
+
+    @Override
+    protected void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        g.setColor(Color.yellow);
+        g.drawString(this.name,this.xPos,this.yPos);
+
+    }
+}
