88b974b83a8f6f88f5f0af8c0e276b6dc07627e0
htimsnhoj543678 <54507190+htimsnhoj543678@users.noreply.github.com>
2021-11-22 18:11:27 -0500

    Update Node.java

--- src/monitoring/tool/Node.java
+++ src/monitoring/tool/Node.java
@@ -1,6 +1,5 @@
 package monitoring.tool;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Node
 {
@@ -14,7 +13,7 @@ public class Node
     private boolean outbreakStatus = false;                         //default false or true if node has more than 4 injections of the same virus in 4 mins
     private int numOfConnections;                                   //number of other nodes this node is connected to
     private ArrayList<String> connections = new ArrayList<>();                                     //list of connected nodes
-    private ArrayList<Attack> attacks = new ArrayList<>();                                       //list of attacks
+    //private Attack[] attacks;                                       //list of attacks
     //private ArrayList<String> attackList = new ArrayList<String>(); //type of attack: red, blue, yellow, or black, can then count # of attacks from this
     private int numOfAttacks;                                       //number of attacks on this node
     //private ArrayList<Node> badConnections;                         //list of connected nodes which are infected
@@ -68,7 +67,7 @@ public class Node
     public ArrayList<String> getConnections() {return connections;}
 
     //public String getAttackType() {return attackType;}
-    public int getNumOfConnections() {return numOfConnections;}
+    public int getNumOfConnections() {return this.numOfConnections;}
     public int getNumOfAttacks() {return numOfAttacks;}
     //public ArrayList getBadConnections() {return badConnections;}
     //public ArrayList getFirewallLog() {return firewallLog;}
@@ -78,17 +77,13 @@ public class Node
     public void setFirewallStatus(boolean status) {this.firewallStatus = status;}
     public void setOutbreakStatus(boolean status) {this.outbreakStatus = status;}
     public void setXY(int x, int y){this.xpos=x; this.ypos=y;}
-    public void setAttacks(Attack attack) {
-        this.attacks.add(attack);
-    }
+    public void setNumConnections()
+    {
+        this.numOfConnections = 0;
+        for(int i = 0; i < this.connections.size();i++)
+            this.numOfConnections++;
+    };
 
-    //print attack
-    public void printAttacks(){
-        for (int i = 0; i < attacks.size(); i++) {
-            Attack cAttack = attacks.get(i);
-            System.out.println("Attack:: Name: " + cAttack.getName() + " Colour: " + cAttack.getColorType() + " Date: " + cAttack.getDate() + " Time: " + cAttack.getTime());
-        }
-    }
     //functions
     public void insertConnection(String node)
     {
