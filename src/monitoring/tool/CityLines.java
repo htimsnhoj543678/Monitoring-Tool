d55196530eaf734935d6d94327bba8c99cda464a
Mitchell Zinck <58631706+Zinckle@users.noreply.github.com>
2021-12-09 19:14:22 -0500

    Commented CityLines

--- src/monitoring/tool/CityLines.java
+++ src/monitoring/tool/CityLines.java
@@ -4,13 +4,23 @@ import javax.swing.*;
 import java.awt.*;
 
 public class CityLines extends JLabel {
-
+    /**
+     * variables of the CityLines class
+     */
     private int x1;
     private int y1;
     private int x2;
     private int y2;
     private Color color;
 
+    /**
+     * constructor of a CityLines object
+     * @param x1 first x coordinate
+     * @param y1 first y coordinate
+     * @param x2 second x coordinate
+     * @param y2 second y coordinate
+     * @param color colour of the node
+     */
     public CityLines(int x1, int y1, int x2, int y2, Color color)
     {
         this.x1 = x1;
@@ -21,12 +31,19 @@ public class CityLines extends JLabel {
 
     }
 
+    /**
+     *getters for the variables
+     */
     public int getX1(){return x1;}
     public int getY1(){return y1;}
     public int getX2(){return x2;}
     public int getY2(){return y2;}
     public Color getColor() {return color;}
 
+    /**
+     * method for drawing the points representing the cities
+     * @param g graphics element
+     */
     @Override
     protected void paintComponent(Graphics g)
     {
